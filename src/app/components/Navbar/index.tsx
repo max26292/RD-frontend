cc2b8c42227f271a8097dd3b9bcc095b764ad69a
MadJoker <hoang.nguyennguyen@aipacific.vn>
2020-11-14 19:57:44 +0700

    update

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as FaiIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
@@ -36,6 +36,7 @@ export function Navbar() {
       </li>
     );
   });
+
   return (
     <>
       <div className="navbar">
@@ -57,5 +58,58 @@ export function Navbar() {
     </>
   );
 }
-
+// CamelCase;
+// lower_case;
 //  default Navbar;
+interface stateInterface {
+  sidebar: boolean;
+}
+interface myProps {}
+class SideBar extends Component<myProps, stateInterface> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebar: false,
+    };
+    this.showSidebar = this.showSidebar.bind(this);
+  }
+  showSidebar() {
+    this.setState({
+      sidebar: !this.state.sidebar,
+    });
+  }
+  componentDidMount() {}
+  render() {
+    const navItem = NavbarItem.map((value, index) => {
+      console.debug({ value });
+      return (
+        <li className="navbar-toggle" key={value.key}>
+          <Link to={`${value.link}`} className="menu-bar">
+            {/* <AiIcons.AiOutlineClose /> */}
+            {value.name}
+          </Link>
+        </li>
+      );
+    });
+    return (
+      <>
+        <div className="navbar">
+          <Link to="#" className="menu-bar">
+            <FaiIcons.FaBars onClick={this.showSidebar} />
+          </Link>
+        </div>
+
+        <nav className={this.state.sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className="nav-menu-items">
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bar">
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {navItem}
+          </ul>
+        </nav>
+      </>
+    );
+  }
+}
